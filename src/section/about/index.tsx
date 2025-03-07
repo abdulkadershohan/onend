9bb6de10c7f2aea33847546a5f107b86d47cb8db
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-03-08 00:42:26 +0600

    enhance About section; add content and button for consultation

--- src/section/about/index.tsx
+++ src/section/about/index.tsx
@@ -1,10 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function About() {
     return (
         <section >
-            <div className="container mx-auto -mt-70 ">
+            <div className="container mx-auto -mt-70 space-y-10 ">
                 <img src='/images/video.png' alt="video" className="w-full h-[802px] relative z-20 " />
+                <div className="text-center space-y-10 ">
+                    <span className="uppercase">
+                        Who We Are
+                    </span>
+                    <p
+                        className="font-medium text-3xl md:text-5xl leading-10 md:leading-15 "
+                    >
+                        Onend is a digital solution for a product agency
+                        that relates people relations with products, story development, and other services.
+
+                    </p>
+                    <Button
+                        className="bg-[#B6F214] text-[#000] rounded-none cursor-pointer md:h-16 md:w-63 "
+                    >
+                        Get a free consultation <ArrowRight className="ml-2" />
+                    </Button>
+                </div>
             </div>
+
+
         </section>
     )
 }
